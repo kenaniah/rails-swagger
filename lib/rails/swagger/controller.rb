82f94c489feb099e8d3655e1beb8bfb5bcaedd3f
Kenaniah Cerny <kenaniah@gmail.com>
2017-05-17 14:04:10 -0700

    removes incomplete code

--- lib/rails/swagger/controller.rb
+++ lib/rails/swagger/controller.rb
@@ -2,13 +2,19 @@ module Rails
 	module Swagger
 		module Controller
 
+			# METHODS_WITH_BODIES = [
+			# 	:post,
+			# 	:patch,
+			# 	:put
+			# ].freeze
+
 			# Injects swagger-related code into the controller when included
 			def self.included base
 
 				# Add controller hooks
-				base.class_eval do
-					before_action :validate_swagger_params
-				end
+				# base.class_eval do
+				# 	before_action :swagger_validate_params
+				# end
 
 				# Returns the swagger spec definition for the endpoint serving
 				# the current request.
@@ -19,9 +25,12 @@ module Rails
 
 				# Validates request parameters against the Swagger API spec
 				# associated with this controller.
-				def validate_swagger_params
-					#puts swagger_endpoint.inspect.white
-				end
+				# def swagger_validate_params
+				# 	if METHODS_WITH_BODIES.include? request.method_symbol
+				# 		body = request.POST
+				# 		# TODO: add validation here
+				# 	end
+				# end
 
 			end
 
--- lib/rails/swagger/version.rb
+++
@@ -1,5 +0,0 @@
-module Rails
-  module Swagger
-    VERSION = '0.1.0'
-  end
-end
